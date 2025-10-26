bd78ce8383db270b50db13c1a3c89b1828b4f353
Brandon Niu <niubrandon@gmail.com>
2025-10-26 06:49:29 -0400

    setup the bootstrap project using lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-backend
+
+TODO: write [great documentation](https://jacobian.org/writing/what-to-write/)
---
+++ test/clj_backend/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-backend.core-test
+  (:require [clojure.test :refer :all]
+            [clj-backend.core :refer :all]))
+
+(deftest a-test
+  (testing "Basic test that should pass."
+    (is (= 1 1))))
