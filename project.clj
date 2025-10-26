035fd82b9e6eeaa6a6ea67c844c14eb2d55c6c35
Brandon Niu <niubrandon@gmail.com>
2025-10-26 07:17:29 -0400

    create a simple server

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject clj-backend "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A backend for Defy.ai"
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [compojure "1.7.0"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring-defaults "0.4.0"]
+                 [ring/ring-json "0.5.1"]
+                 [cheshire "5.12.0"]
+                 [http-kit "2.8.0"]]
   :main ^:skip-aot clj-backend.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/clj_backend/core.clj
+++ src/clj_backend/core.clj
@@ -1,7 +1,78 @@
 (ns clj-backend.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [org.httpkit.server :refer [run-server]]
+            [cheshire.core :as json])
   (:gen-class))
 
+;; In-memory storage for bikes
+(def bikes (atom {}))
+
+;; Bike data model
+(defn create-bike [id brand model year color price]
+  {:id id
+   :brand brand
+   :model model
+   :year year
+   :color color
+   :price price})
+
+;; Routes
+(defroutes app-routes
+  ;; POST /bikes - Create a new bike
+  (POST "/bikes" request
+    (let [body (:body request)
+          id (str (java.util.UUID/randomUUID))
+          bike (create-bike id
+                           (:brand body)
+                           (:model body)
+                           (:year body)
+                           (:color body)
+                           (:price body))]
+      (swap! bikes assoc id bike)
+      {:status 201
+       :headers {"Content-Type" "application/json"}
+       :body (json/generate-string bike)}))
+
+  ;; GET /bikes - Get all bikes
+  (GET "/bikes" []
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+       :body (json/generate-string (vals @bikes))})
+
+  ;; GET /bikes/:id - Get a specific bike
+  (GET "/bikes/:id" [id]
+    (if-let [bike (get @bikes id)]
+      {:status 200
+       :headers {"Content-Type" "application/json"}
+       :body (json/generate-string bike)}
+      {:status 404
+       :headers {"Content-Type" "application/json"}
+       :body (json/generate-string {:error "Bike not found"})}))
+
+  ;; Health check endpoint
+  (GET "/health" []
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body (json/generate-string {:status "ok" :message "Server is running"})})
+
+  ;; Catch-all for undefined routes
+  (route/not-found
+    {:status 404
+     :headers {"Content-Type" "application/json"}
+     :body (json/generate-string {:error "Not found"})}))
+
+;; Main app with middleware
+(def app
+  (-> app-routes
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start the bike server"
   [& args]
-  (println "Hello, World!"))
+  (println "Starting bike server on port 3000...")
+  (run-server app {:port 3000}))
